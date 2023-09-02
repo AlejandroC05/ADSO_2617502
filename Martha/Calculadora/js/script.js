df24180643848bf6f6ce6faf4f18ac09b67e28e2
Estefania <yeineralajandro3@gmail.com>
2023-09-02 16:50:43 -0500

    Se añadió una Calculadora hecha con JavaScript

---
+++ Martha/Calculadora/js/script.js
@@ -0,0 +1,48 @@
+function suma (){
+    var a, b
+    a = Number(prompt("Digite el primer número"));
+    b = Number(prompt("Digite el segundo número"));
+    primera = (a + b)
+    alert("La suma es igual a: " + primera)
+}
+
+function resta(){
+    var c, d
+    c = Number(prompt("Digite el primer número"));
+    d = Number(prompt("Digite el segundo número"));
+    segunda = (c - d)
+    alert("La diferencia es igual a: " + segunda)
+}
+
+function multiplicacion(){
+    var e, f
+    e = Number(prompt("Digite el primer número"));
+    f = Number(prompt("Digite el segundo número"));
+    tercera = (e * f)
+    alert("El producto es igual a: " + tercera)
+}
+
+function division(){
+    var g, h
+    g = Number(prompt("Digite el primer número"));
+    h = Number(prompt("Digite el segundo número"));
+    curta = (e / f)
+    alert("El cociente es igual a: " + curta)
+}
+
+function promedio(){
+    var i, j, k
+    i = Number(prompt("Escriba el primer número"));
+    j = Number(prompt("Digite el segundo número"));
+    k = Number(prompt("Ingrese el tercer número"));
+    quinta = (i+j+k)/3
+    alert("El promedio es igual a: " + quinta)
+}
+
+function porcentaje(){
+    var e, f
+    e = Number(prompt("Digite el primer número"));
+    f = Number(prompt("Digite el segundo número"));
+    sexta = (e * f)
+    alert("El producto es igual a: " + sexta)
+}
